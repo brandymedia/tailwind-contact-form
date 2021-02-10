a397a3439e3df0a03d0868d695ebd13ea69e7a90
Andy Griffiths <andygriffiths@Andys-MacBook-Pro.local>
2021-02-10 11:24:04 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './public/**/*.js'
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
